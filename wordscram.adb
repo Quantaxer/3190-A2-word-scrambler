84ce1f521304bfb895aba4d56a694b331be5fa02
Phudel <phudel@uoguelph.ca>
2020-01-10 14:53:23 -0500

    Finish parsing text file into words

--- wordscram.adb
+++ wordscram.adb
@@ -7,6 +7,7 @@ procedure wordscram is
 	fileName : unbounded_string;
 	temp: integer;
 
+
 	-- Helper function for getFilename: determines if a file exists or not
 	-- Param: fileName: The filename to check if it exists
 	-- Return: Boolean
@@ -44,12 +45,37 @@ procedure wordscram is
 	function processText(fileName : string) return integer is
 		infp: file_type;
 		line: unbounded_string;
+		i : integer;
+		wordStart, wordLen: integer;
 	begin
 		open(infp, in_file, fileName);
 		loop
 			exit when end_of_file(infp);
 			line := get_line(infp);
-			put(line);
+
+			wordLen := 0;
+			wordStart := 1;
+			i := 1;
+
+			-- Loop through line in the file, splitting the line by words
+			-- This is done by keeping track of the word's start index and length
+			for i in 1..length(line) loop
+				if Element(line, i) = ' ' or Element(line, i) = '.' or Element(line, i) = ',' then
+					if wordLen > 0 then
+						put_line(unbounded_slice(line, wordStart, wordStart + wordLen - 1));
+					end if;
+					wordStart := i + 1;
+					wordLen := 0;
+				else 
+					wordLen := wordLen + 1;
+				end if;
+			end loop;
+
+			-- This is needed because if the line doesn't end with a delimiter, it will skip that word.
+			if wordLen > 0 then
+				put_line(unbounded_slice(line, wordStart, wordStart + wordLen - 1));
+			end if;
+
 		end loop;
 
 		return 1;
